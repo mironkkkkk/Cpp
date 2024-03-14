1e3ab5dd7675c5f0419b9c1945f82aac10fca01f
Mmir0 <163337961+Mmir0@users.noreply.github.com>
2024-03-14 20:33:59 +0300

    Add files via upload

---
+++ !5HomeWork/!5HomeWork.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // ex1
+    const int size1 = 5;
+    const int size2 = 5;
+    int num;
+    cout << "enter num: ";
+    cin >> num;
+    int arr[size1][size2] = {};
+    for (int i = 0, number = num; i < size1; i++) {
+        for (int j = 0; j < size2; j++) {
+            arr[i][j] = number;
+            number *= 2;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    // ex2
+    const int size3 = 4;
+    const int size4 = 4;
+    int arr2[size3][size4] = {};
+    int num2;
+    cout << "enter num: ";
+    cin >> num2;
+    for (int i = 0, count = num2; i < size3; i++) {
+        for (int j = 0; j < size4; j++) {
+            arr2[i][j] = num2;
+            num2++;
+            cout << arr2[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
