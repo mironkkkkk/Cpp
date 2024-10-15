2cdd3df72417fe2a93a4616ba900444dd64d09a3
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2024-10-15 20:53:07 +0300

    Add files via upload

---
+++ classWorkWorldFile/ConsoleApplication3.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+void readFile(string path1, string path2) {
+	string line;
+	ifstream in(path1);
+	ofstream out(path2);
+	if (in.is_open()) {
+		while (in >> line) {
+			if (line.length() >= 7) {
+				out << line << " ";
+			}
+		}
+	}
+	in.close();
+	out.close();
+}
+
+void cloneFile(string path1, string path2) {
+	string line;
+	ifstream in(path1);
+	ofstream out(path2);
+	if (in.is_open()) {
+		while (getline(in, line)) {
+			out << line << endl;
+		}
+	}
+	in.close();
+	out.close();
+}
+
+void cloneFileReverse(string path1, string path2) {
+	string line;
+	ifstream in(path1);
+	ofstream out(path2);
+	if (in.is_open()) {
+		while (getline(in, line)) {
+			out << line << endl;
+		}
+	}
+	in.close();
+	out.close();
+}
+
+
+
+
+
+int main()
+{
+	string path1 = "test.txt";
+	string path2 = "testClone.txt";
+	//readFile(path1, path2);
+	cloneFile(path1, path2);
+}
+
+
+
+
